badcf18bb0dfc9e1e6d21c06c9244188aa8232f6
enzoderenzis <enzo.derenzis@nantech.com.ar>
2021-11-05 16:42:28 -0300

    add google login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
 import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
 import  CharacterData  from './features/characterPersonalData/CharacterData';
 import CharacterLocation from './features/characterLocation/CharacterLocation';
 import ViewCharacter  from  './features/viewCharacter/ViewCharacter';
 import CharacterList from './features/global/CharacterList';
+import {
+  setUser,
+  cleanUser,
+} from './features/global/global.slice';
 import './App.css';
 
+const googleClientId = "790472503871-862ja67k2gsncq2vvmn1pa748m9f3nhi.apps.googleusercontent.com";
+
 function App() {
+  const dispatch = useDispatch();
   const position = useSelector( state => state.global.position );
-  console.log({position})
+  const user = useSelector( state => state.global.user );
+
+  const onSuccessLogin = (response) => {
+    console.log("========== onSuccess ==============", response)
+    console.log("response.profileObj", response.profileObj)
+    if(response && response.accessToken && response.profileObj) {
+      dispatch(setUser({...response.profileObj, accessToken: response.accessToken}))
+    }
+  };
+  const onErrorLogin = (response) => {
+    console.log("========== onError ==============", response);
+    dispatch(cleanUser());
+  };
+  console.log({user})
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
+        {!user ?
+          <GoogleLogin
+              clientId={googleClientId}
+              buttonText="Log in with Google"
+              onSuccess={onSuccessLogin}
+              onFailure={onErrorLogin}
+              cookiePolicy={'single_host_origin'}
+          />
+        :
+        <>
         {position === 0 &&  <CharacterData />}
         {position === 1 &&  <CharacterLocation />}
         {position === 2 &&  <ViewCharacter />}
         {position === 3 &&  <CharacterList />}
+        <GoogleLogout
+            clientId={googleClientId}
+            buttonText="Logout"
+            onLogoutSuccess={() => dispatch(cleanUser()) }
+          />
+        </>
+        }
+
 
 
         <p>
